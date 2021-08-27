96f71bcc04bdd9f200240195420a4235011f420c
Leonardo Braz <lhleonardo05@gmail.com>
2021-08-27 07:53:16 -0300

    Tratamento de erros via middleware

---
+++ src/middlewares/exception-handler.ts
@@ -0,0 +1,17 @@
+import { NextFunction, Request, Response } from "express";
+
+import { AppError } from "../errors/app-error";
+
+export function exceptionHandler(
+    error: Error,
+    _: Request,
+    response: Response,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    next: NextFunction
+): Response {
+    if (error instanceof AppError) {
+        return response.status(error.statusCode).json({ error: error.message });
+    }
+
+    return response.status(500).json({ error: "Internal server error" });
+}
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,8 @@
 import express from "express";
 
+import "express-async-errors";
+
+import { exceptionHandler } from "./middlewares/exception-handler";
 import { routes } from "./routes";
 
 const app = express();
@@ -9,5 +12,7 @@ const port = 3000;
 app.use(express.json());
 
 app.use(routes);
+
+app.use(exceptionHandler);
 
 app.listen(port, () => console.log(`Servidor iniciado na porta ${port}`));
