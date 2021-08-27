9805e41ed137faf87990fe80ed9c0aa3e1a78bb6
Leonardo Braz <lhleonardo05@gmail.com>
2021-08-26 21:55:57 -0300

    Configurando ts-node-dev

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,7 @@
-import express from "express"
+import express from "express";
 
 const app = express();
 
+const port = 3000;
 
-app.listen(3000,
-    () => {
-
-
-
-
-    }
-)
+app.listen(port, () => console.log(`Servidor iniciado na porta ${port}`));
