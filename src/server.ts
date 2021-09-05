33993061995c90d83243ad484759082228f6787c
Leonardo Braz <lhleonardo05@gmail.com>
2021-09-05 20:33:01 -0300

    Configura o swagger

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,19 @@
-import express from "express";
-
 import "express-async-errors";
+import express from "express";
+import swagger from "swagger-ui-express";
 
 import { exceptionHandler } from "./middlewares/exception-handler";
 import { routes } from "./routes";
+import swaggerConfig from "./swagger.json";
 
 const app = express();
 
 const port = 3000;
 
 app.use(express.json());
-
 app.use(routes);
+
+app.use("/api-docs", swagger.serve, swagger.setup(swaggerConfig));
 
 app.use(exceptionHandler);
 
