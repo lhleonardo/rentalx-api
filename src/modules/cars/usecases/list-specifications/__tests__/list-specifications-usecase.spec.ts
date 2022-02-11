f1e2d5ede62f0c877f89e7410f044e00a8e37db2
Leonardo Braz <lhleonardo05@gmail.com>
2022-02-11 00:06:32 -0300

    test: listagem de especificações

---
+++ src/modules/cars/usecases/list-specifications/__tests__/list-specifications-usecase.spec.ts
@@ -0,0 +1,57 @@
+import { ListSpecificationsUseCase } from "..";
+
+import { FakeSpecificationsRepository } from "../../../repositories/implementations/fake/fake-specifications-repository";
+import { SpecificationsRepository } from "../../../repositories/specifications-repository";
+
+let repository: SpecificationsRepository;
+let usecase: ListSpecificationsUseCase;
+
+describe("List Specifications UseCase", () => {
+    beforeEach(() => {
+        repository = new FakeSpecificationsRepository();
+        usecase = new ListSpecificationsUseCase(repository);
+    });
+
+    it("should be able to returns empty if no specification exists", () => {
+        const response = usecase.execute();
+        expect(response).resolves.toEqual([]);
+    });
+
+    it("should be able to return the correct items", async () => {
+        const spec1 = await repository.create({
+            name: "specification-1",
+            description: "some-description",
+        });
+        expect(await usecase.execute()).toEqual(
+            expect.arrayContaining([spec1])
+        );
+
+        const spec2 = await repository.create({
+            name: "specification-2",
+            description: "some-description",
+        });
+        expect(await usecase.execute()).toEqual(
+            expect.arrayContaining([spec1, spec2])
+        );
+    });
+
+    it("should be able to return the correct count of items", async () => {
+        const operations = [];
+        for (let i = 1; i <= 10; i += 1) {
+            const promise = repository.create({
+                name: `specification-${i}`,
+                description: "some-description",
+            });
+
+            operations.push(promise);
+        }
+
+        Promise.all(operations);
+
+        const response = await usecase.execute();
+
+        expect(response).toBeTruthy();
+        expect(response).toBeInstanceOf(Array);
+        expect(response.length).toBe(10);
+    });
+});
